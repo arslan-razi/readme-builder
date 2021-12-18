47ec83e67f668b35377ae14397fc42fa72395006
Arslan Razi <arslan.razi@outlook.com>
2021-12-18 18:34:52 -0500

    License badge lookup based on selection

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,46 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// this returns a license badge based on which license was selected
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
-
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
-
-// TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
-
-`;
+const addBadge = licenseChoice => {
+  if (licenseChoice == 'Apache License 2.0') {
+    return `
+![apache](https://img.shields.io/badge/license-Apache%20License%202.0-blue)
+    `;
+  } else if (licenseChoice == 'GNU GPLv3') {
+    return `
+![gnu](https://img.shields.io/badge/license-GNU%20GPLv3-green)
+    `;
+  } else if (licenseChoice == 'ISC License') {
+    return `
+![isc](https://img.shields.io/badge/license-ISC%20License-purple)
+    `;
+  } else {
+    return `
+![mit](https://img.shields.io/badge/license-MIT%20License-red)
+    `;
+  }
 }
+
+// this checks to see what license was selected and updates the License section accordingly
+const checkLicense = licenseChoice => {
+  if (licenseChoice == 'Apache License 2.0') {
+    return `
+This application is covered by the Apache v2.0 License, the terms of which can be found [here](https://www.apache.org/licenses/LICENSE-2.0.txt).
+    `;
+  } else if (licenseChoice == 'GNU GPLv3') {
+    return `
+This application is covered by the GNU General Purpose License, the terms of which can be found [here](https://www.gnu.org/licenses/gpl-3.0.en.html).
+    `;
+  } else if (licenseChoice == 'ISC License') {
+    return `
+This application is covered by the ISC License, the terms of which can be found [here](https://opensource.org/licenses/ISC).
+    `;
+  } else {
+    return `
+This application is covered by the MIT License, the terms of which can be found [here](https://opensource.org/licenses/MIT).
+    `;
+  }
+}
+
+
 
 module.exports = generateMarkdown;
